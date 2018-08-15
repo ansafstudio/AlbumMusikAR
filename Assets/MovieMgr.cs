99302f1bed3a21c8c4a6a396da29caf96a18edc7
Muhammad Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-08-15 15:09:19 +0700

    Commit Project
    Commit Project

---
+++ Assets/MovieMgr.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieMgr : MonoBehaviour {
+
+    private string movie = "GangnamStyle.mp4";
+    //public int sceneNumber;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(streamVideo(movie));
+    }
+
+    private IEnumerator streamVideo(string video)
+    {
+        Handheld.PlayFullScreenMovie(video, Color.black, FullScreenMovieControlMode.Full, FullScreenMovieScalingMode.AspectFill);
+        yield return new WaitForEndOfFrame();
+        Debug.Log("The Video playback is now completed.");
+        //Application.loadedLevel
+        //SceneManager.LoadScene(sceneNumber);
+    }
+}
